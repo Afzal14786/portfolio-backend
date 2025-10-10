ad0695dc25a216b7a155087c0a559cf878560a5a
Afzal14786 <mdafzal14777@gmail.com>
2025-10-10 22:41:03 +0530

    updating verify-otp route for login

--- src/routes/user/login.user.route.js
+++ src/routes/user/login.user.route.js
@@ -1,10 +1,20 @@
 import express from "express";
 const router = express.Router();
 import {login, logout} from "../../controllers/user/login.user.controller.js";
+import {resetPassword, verifyReset} from "../../controllers/user/update.password.controller.js";
+
+import { verifyOtp } from "../../controllers/user/verifyOtp.user.controller.js";
 
 
 // login routes
 router.post("/login", login);
 router.post("/logout", logout);
+/**
+ * The login feature is not implemented successfully as well as there is significant issues in register and login
+ * follow up pendings
+ */
+router.post('/verify-otp', verifyOtp);
+router.post("/forgot-password", resetPassword);
+router.post("/reset-password", verifyReset);
 
 export default router;
